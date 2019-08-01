1816c92bb36672fa35679621e5e42e8fe89bde05
xdyi <409070169@qq.com>
2019-08-01 12:18:35 +0800

    wangge

--- new_fc_mining.py
+++ new_fc_mining.py
@@ -462,7 +462,7 @@ if __name__ == '__main__':
 
     load_money = "usdt"
     total_load_coin="eos eth ltc trx etc zec xlm ada btc bch xrp ft ada dash bsv iota"
-    load_coin = "ltc"
+    load_coin = "ltc eth eos"
     load_parition="2"
     load_total_money="100"
     load_bidirection="3"
