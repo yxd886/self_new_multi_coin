f60573fd5e877a78fd3616ff26781c8d8edc6d96
定逺 <dingyuan.yxd@alibaba-inc.com>
2019-08-06 09:03:20 +0800

    aa

--- new_fc_mining.py
+++ new_fc_mining.py
@@ -392,7 +392,7 @@ if __name__ == '__main__':
 
 
     load_money = "usdt"
-    total_load_coin="eos eth ltc trx etc zec xlm ada btc bch xrp ft ada dash bsv iota"
+    total_load_coin="eos eth ltc trx etc zec xlm ada btc bch xrp ft ada dash bsv iota fmex"
     load_coin = "eos etc ltc"
     load_parition="2"
     load_total_money="100"
