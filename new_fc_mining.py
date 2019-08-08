b1cff7f8b88e6bc7098819eb38ab13740a6c168a
定逺 <dingyuan.yxd@alibaba-inc.com>
2019-08-09 00:50:18 +0800

    aa

--- new_fc_mining.py
+++ new_fc_mining.py
@@ -135,8 +135,8 @@ def buy_main_body(mutex2, api, bidirection, partition, _money, _coin, min_size,
             # if need_sell:
             #    api.take_order(market, "sell", ask1, min_size, coin_place)
             money, coin, freez_money, freez_coin = api.get_available_balance(_money, _coin)
-            real_buy_step_size = 2*min_size
-            real_sell_step_size = 2*min_size
+            real_buy_step_size = 10*min_size
+            real_sell_step_size = 10*min_size
 
             if need_buy:
                 api.take_order(market, "buy", buy1, real_buy_step_size, coin_place)
